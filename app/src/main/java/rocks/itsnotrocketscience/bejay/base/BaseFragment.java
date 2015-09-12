d49187d141ab4a43bb069ceeae2560c1a4a0c9de
= <liam.j.duffy5@gmail.com>
2015-09-12 17:26:30 +0100

    added event list

--- app/src/main/java/rocks/itsnotrocketscience/bejay/base/BaseFragment.java
+++ app/src/main/java/rocks/itsnotrocketscience/bejay/base/BaseFragment.java
@@ -2,6 +2,10 @@ package rocks.itsnotrocketscience.bejay.base;
 
 import android.app.Activity;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import rocks.itsnotrocketscience.bejay.R;
 
 /**
  * Created by centralstation on 11/09/15.
@@ -15,5 +19,13 @@ public class BaseFragment extends Fragment{
             return null;
         else
             return (AppApplication)activity.getApplication();
+    }
+
+    public void showFragment(Fragment fragment)
+    {
+        FragmentManager manager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/rocks/itsnotrocketscience/bejay/home/HomeFragment.java
+++ app/src/main/java/rocks/itsnotrocketscience/bejay/home/HomeFragment.java
@@ -5,11 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import at.markushi.ui.CircleButton;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rocks.itsnotrocketscience.bejay.R;
 import rocks.itsnotrocketscience.bejay.base.BaseFragment;
+import rocks.itsnotrocketscience.bejay.event.EventListFragment;
 
 public class HomeFragment extends BaseFragment {
-
+    @Bind(R.id.btFindEvents)
+    CircleButton btFindEvents;
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
@@ -21,7 +27,14 @@ public class HomeFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.btFindEvents)
+    public void findEvents() {
+        showFragment(EventListFragment.newInstance());
     }
 
     public static String getTitle() {
