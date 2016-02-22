f6cb56cfbff084870f0a1f53a3dbdf0e85d47f9f
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-02-22 19:30:16 +0000

    fixed conflicts. changed build config, uppdated facebook application.

--- app/src/main/java/rocks/itsnotrocketscience/bejay/api/Constants.java
+++ app/src/main/java/rocks/itsnotrocketscience/bejay/api/Constants.java
@@ -13,16 +13,9 @@ public interface Constants {
     String EMAIL = "email";
     String USERNAME = "username";
     String BASE_URL = BuildConfig.BASE_URL;
-<<<<<<< HEAD
-    String BASE_API = "dukebox/";
-    String API = BuildConfig.BASE_URL + BASE_API;
-    String ACCESS_TOKEN = "access_token";
     String TOKEN_TYPE = "token_type";
     String REFRESH_TOKEN = "refresh_token";
     String IS_LOGGED_IN = "is_logged_in";
-=======
-    String API = BuildConfig.BASE_URL;
-    String TOKEN_AUTH = "api-token-auth";
+    String API = BuildConfig.BASE_URL + "dukebox/";
     LogLevel RETROFIT_LOG_LEVEL = BuildConfig.RETROFIT_LOG_LEVEL;
->>>>>>> 3da375a164ebebaa72bcb66b23025f3b0f27fc79
 }
