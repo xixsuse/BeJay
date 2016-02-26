85a2341b647bef5c74ff11c896df9047032992a3
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-02-26 19:43:00 +0000

    added like api

--- app/src/main/java/rocks/itsnotrocketscience/bejay/models/Song.java
+++ app/src/main/java/rocks/itsnotrocketscience/bejay/models/Song.java
@@ -4,10 +4,11 @@ package rocks.itsnotrocketscience.bejay.models;
  * Created by centralstation on 20/08/15.
  */
 
-public class Song extends BaseModel {
+public class Song{
 
     private String title;
-    private int count;
+    private int id;
+    private int likes;
     private Boolean chosen;
 
     public Song(String title) {
@@ -22,23 +23,11 @@ public class Song extends BaseModel {
         this.title = title;
     }
 
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public Boolean getChosen() {
-        return chosen;
-    }
-
-    public void setChosen(Boolean chosen) {
-        this.chosen = chosen;
-    }
-
     public String getCountStr() {
-        return String.valueOf(count);
+        return String.valueOf(likes);
+    }
+
+    public int getId() {
+        return id;
     }
 }
