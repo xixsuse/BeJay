4794fc1153812e50e5b7d8feb9b74ff2fe1ad3a3
Liam Duffy <liam.duffy@synchronoss.com>
2016-11-20 19:52:51 +0000

    cleaned up presenter

--- app/src/main/java/rocks/itsnotrocketscience/bejay/event/single/EventPresenterImpl.java
+++ app/src/main/java/rocks/itsnotrocketscience/bejay/event/single/EventPresenterImpl.java
@@ -5,51 +5,50 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 
-import com.cantrowitz.rxbroadcast.RxBroadcast;
-
 import javax.inject.Inject;
+
+import com.cantrowitz.rxbroadcast.RxBroadcast;
 
 import rocks.itsnotrocketscience.bejay.api.Constants;
 import rocks.itsnotrocketscience.bejay.api.retrofit.Events;
-import rocks.itsnotrocketscience.bejay.models.Event;
 import rocks.itsnotrocketscience.bejay.models.Like;
 import rocks.itsnotrocketscience.bejay.models.Song;
 import rx.Observable;
-import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 /**
  * Created by sirfunkenstine on 12/02/16.
- *
  */
 public class EventPresenterImpl implements EventContract.EventPresenter {
 
-    private static final String TAG = "EventPresenterImpl";
     private EventContract.EventView view;
     private final SharedPreferences preferences;
     private Subscription subscription;
     private final Events event;
 
     @Inject
-    public EventPresenterImpl(SharedPreferences preferences, Events event){
-        this.preferences=preferences;
-        this.event=event;
+    public EventPresenterImpl(SharedPreferences preferences, Events event) {
+        this.preferences = preferences;
+        this.event = event;
         view = null;
     }
 
     @Override
     public void onViewAttached(EventContract.EventView view) {
-        this.view=view;
-    }
-
-    public void onViewDetached() {
-        this.view=null;
+        this.view = view;
     }
 
     @Override
-    public void onDestroy() {}
+    public void onViewDetached() {
+        view = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        //not implemented
+    }
 
     @Override
     public void loadEvent(int id) {
@@ -57,21 +56,9 @@ public class EventPresenterImpl implements EventContract.EventPresenter {
         event.get(id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Event>() {
-                    @Override
-                    public void onCompleted() {
-                        view.setProgressVisible(false);
-                    }
-                    @Override
-                    public final void onError(Throwable e) {
-                        view.showToast(e.toString());
-                        view.setProgressVisible(false);
-                    }
-                    @Override
-                    public final void onNext(Event response) {
-                        view.onEventLoaded(response);
-                    }
-                });
+                .subscribe(response -> view.onEventLoaded(response),
+                        e -> view.setProgressVisible(false),
+                        () -> view.setProgressVisible(false));
     }
 
     @Override
@@ -79,53 +66,24 @@ public class EventPresenterImpl implements EventContract.EventPresenter {
         event.postSong(song)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Song>() {
-                    @Override
-                    public void onCompleted() {}
-                    @Override
-                    public final void onError(Throwable e) {
-                        view.showToast(e.toString());
-                    }
-                    @Override
-                    public final void onNext(Song response) {
-                        view.onSongAdded(response);
-                    }
-                });
+                .subscribe(response -> view.onSongAdded(response),
+                        e -> view.showToast(e.toString()));
     }
 
-    private void addLike(final Song song,int pos) {
+    private void addLike(Song song, int pos) {
         event.postLike(new Like(song)).subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Like>() {
-                    @Override
-                    public void onCompleted() {}
-                    @Override
-                    public final void onError(Throwable e) {
-                        view.showToast(e.toString());
-                    }
-                    @Override
-                    public final void onNext(Like response) {
-                        updateLikeUi(song, response, pos);
-                    }
-                });
+                .subscribe(response -> updateLikeUi(song, response, pos),
+                        e -> view.showToast(e.toString()));
     }
 
-    private void deleteLike(final Song song, int pos) {
+    private void deleteLike(Song song, int pos) {
         event.deleteLike(song.getLikeId()).subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Like>() {
-                    @Override
-                    public void onCompleted() {}
-                    @Override
-                    public final void onError(Throwable e) {
-                        view.showToast(e.toString());
-                    }
-                    @Override
-                    public final void onNext(Like response) {
-                        updateLikeUi(song,response, pos);
-                    }
-                });
+                .subscribe(response -> updateLikeUi(song, response, pos),
+                        e -> view.showToast(e.toString()));
     }
+
     @Override
     public void toggleLike(Song song, int pos) {
         if (song.hasLikeId()) {
@@ -136,15 +94,15 @@ public class EventPresenterImpl implements EventContract.EventPresenter {
     }
 
     private void updateLikeUi(Song song, Like response, int pos) {
-        song.updateLikes(response!=null ? 1 :-1);
-        song.updateLiked(response!=null ? response.getId() :-1);
+        song.updateLikes((null != response) ? 1 : -1);
+        song.updateLiked((null != response) ? response.getId() : -1);
         view.notifyItemChanged(pos);
     }
 
     @Override
     public void registerUpdateReceiver(Context context) {
         Observable<Intent> observable = RxBroadcast.fromBroadcast(context, new IntentFilter(EventActivity.EVENT_RECEIVER_ID));
-        subscription= observable.subscribe(s -> {
+        subscription = observable.subscribe(s -> {
             view.showToast("gcm message received");
             loadEvent(preferences.getInt(Constants.EVENT_PK, -1));
         });
@@ -152,6 +110,6 @@ public class EventPresenterImpl implements EventContract.EventPresenter {
 
     @Override
     public void unregisterUpdateReceiver() {
-       subscription.unsubscribe();
+        subscription.unsubscribe();
     }
 }
